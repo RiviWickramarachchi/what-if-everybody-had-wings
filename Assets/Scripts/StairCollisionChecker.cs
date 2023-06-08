4d6a1ebd5270d08870b1b138b590adacc7c1dcc4
RiviWickramarachchi <wickramarachchi.rivindu@gmail.com>
2023-06-08 16:11:30 +0200

    respawn player added, collision trigger for stairs added

---
+++ Assets/Scripts/StairCollisionChecker.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairCollisionChecker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Collider2D stair;
+    private bool isOnCollider;
+
+    void Update() {
+        if(isOnCollider) {
+            stair.enabled = true;
+        }
+        else {
+            stair.enabled = false;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            isOnCollider = !isOnCollider;
+        }
+    }
+
+    // private void OnTriggerExit2D(Collider2D other)
+    // {
+    //     if(other.CompareTag("Player"))
+    //     {
+    //         isOnCollider = false;
+    //     }
+    // }
+}
